ac1646ebe44451c10cbb40c94f36aca0b4258373
Shilton <shilton.sistemas@gmail.com>
2021-11-03 18:46:58 -0300

    Creating models and configuring EF (Context and Migrations)

---
+++ src/CatalogoAPI/Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CatalogoAPI.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string ImagemUrl { get; set; }
+
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
--- src/CatalogoAPI/Startup.cs
+++ src/CatalogoAPI/Startup.cs
@@ -1,16 +1,11 @@
+using CatalogoAPI.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CatalogoAPI
 {
@@ -26,6 +21,11 @@ namespace CatalogoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string mySqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString))
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
