663c01546cac59d1a63a63fa6f80d29b7712721e
Shilton <shilton.sistemas@gmail.com>
2021-11-17 17:31:37 -0300

    Starting Test Project with xUnit

---
+++ src/CatalogoAPIxUnitTests/CategoriasUnitTestController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using CatalogoAPI.Configuration.AutoMapper;
+using CatalogoAPI.Context;
+using CatalogoAPI.Controllers.V2;
+using CatalogoAPI.DTOs;
+using CatalogoAPI.Repository;
+using CatalogoAPI.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CatalogoAPIxUnitTests
+{
+    public class CategoriasUnitTestController
+    {
+        private readonly IUnitOfWork repository;
+        private readonly IMapper mapper;
+
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+        public static string mySqlConnectionTestStr =
+            "Server=localhost;Port=3306;DataBase=DBCatalogo;User=root;Password=shilton@70;Persist Security Info=False;Connect Timeout=300";
+
+        static CategoriasUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+               .UseMySql(mySqlConnectionTestStr, ServerVersion.AutoDetect(mySqlConnectionTestStr))
+               .Options;
+        }
+
+        public CategoriasUnitTestController()
+        {
+            var configMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            mapper = configMapper.CreateMapper();
+
+            var context = new AppDbContext(dbContextOptions);
+
+            /* Caso fosse utilizar outro banco de dados mocado */
+                //DBUnitTestsMockInitializer db = new DBUnitTestsMockInitializer();
+                //db.Seed(context);
+
+            repository = new UnitOfWork(context);
+        }
+
+        [Fact]
+        public void GetCategorias_Return_OKResult()
+        {
+            //Arrange
+            var controller = new CategoriasController(repository, mapper);
+
+            //Act
+            var data = controller.Get();
+
+            //Assert 
+            Assert.IsType<List<CategoriaDTO>>(data.Value);
+        }
+            
+    }
+
+}
---
+++ src/CatalogoAPIxUnitTests/DBUnitTestsMockInitializer.cs
@@ -0,0 +1,26 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+
+namespace CatalogoAPIxUnitTests
+{
+    class DBUnitTestsMockInitializer
+    {
+        public DBUnitTestsMockInitializer()
+        {
+        }
+
+        public void Seed(AppDbContext context)
+        {
+            context.Categorias.Add
+            (new Categoria { CategoriaId = 1, Nome = "Categoria 1", ImagemUrl = "img-1.jpg" });
+
+            context.Categorias.Add
+            (new Categoria { CategoriaId = 2, Nome = "Categoria 2", ImagemUrl = "img-2.jpg" });
+
+            context.Categorias.Add
+            (new Categoria { CategoriaId = 3, Nome = "Categoria 3", ImagemUrl = "img-3.jpg" });
+        }
+
+    }
+
+}
